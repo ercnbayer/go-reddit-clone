4c622eb3b69e4811c196ce63793409671cc79585
EmreErcanBayer <eercan.bayer@gmail.com>
2024-01-02 18:11:34 +0300

    WIP:api and db seperated again

---
+++ api/user.go
@@ -0,0 +1,148 @@
+package api
+
+import (
+	db "emreddit/db"
+	"emreddit/logger"
+	"emreddit/validator"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type UserUpdatePayload struct { //payload for updating user
+	ID       string
+	Name     string `validate:"omitempty,required"` /* if struct field not empty validate */
+	Password string `validate:"omitempty,required"`
+	Email    string `validate:"omitempty,required,email"`
+}
+
+func mapUserUpdatePayloadToDbUser(user *UserUpdatePayload, dbUser *db.UserEntity) {
+
+	dbUser.ID = user.ID
+	dbUser.Name = user.Name
+	dbUser.Email = user.Email
+	dbUser.Password = user.Password
+}
+
+func getUser(c *fiber.Ctx) error {
+
+	//getting single user
+
+	// Get the ID from the URL parameter
+	id := c.Params("id") // getting id from params
+
+	err := validator.ValidateUUID(id) //validating id
+
+	if err != nil {
+
+		logger.Error("invalid req", err)
+
+		return c.Status(400).JSON(err.Error())
+	}
+
+	var user db.UserEntity
+	err = db.ReadUser(id, &user) //readingUser
+
+	if err != nil { //check if err is not null
+
+		logger.Error(err.Error(), err)
+
+		return c.Status(404).JSON(err.Error())
+	}
+
+	return c.JSON(user) // return it as json
+
+}
+func deleteUser(c *fiber.Ctx) error {
+
+	id := c.Params("id") // getting id from params
+
+	err := validator.ValidateUUID(id) //validating id
+
+	if err != nil {
+
+		logger.Error("invalid req", err)
+		return c.Status(400).JSON(err.Error())
+
+	}
+
+	_, err = db.DeleteUser(id) // for delete api
+
+	if err != nil {
+		logger.Error(err.Error())
+		return c.Status(400).JSON(err.Error())
+	}
+
+	return c.JSON(id)
+
+}
+
+func updateUser(c *fiber.Ctx) error {
+
+	id := c.Params("id") //getting id from params
+
+	err := validator.ValidateUUID(id) //validating id
+
+	if err != nil {
+
+		logger.Error("invalid req", err)
+		return c.Status(400).JSON(err.Error())
+
+	}
+
+	var user UserUpdatePayload // creating instance
+
+	if err := c.BodyParser(&user); err != nil { // check if err from body
+
+		logger.Error(" Body Parse error = ", err, user)
+
+		return c.Status(404).JSON(err.Error())
+	}
+	user.ID = id
+
+	if err := validator.Validate.Struct(&user); err != nil { //validating updated values
+
+		logger.Error("validator err= ", err)
+
+		return c.Status(404).JSON(err.Error())
+	}
+
+	var dbUser db.UserEntity
+
+	mapUserUpdatePayloadToDbUser(&user, &dbUser) // maping user to dbUser
+
+	if err := db.UpdateUser(&dbUser); err != nil { //sending it to db
+
+		logger.Error("Update ERR:", err)
+
+		return err
+	}
+
+	return c.Status(200).JSON(user)
+}
+
+func listUsers(c *fiber.Ctx) error {
+
+	people, err := db.GetUsers() //getting all userslist
+
+	if err != nil {
+
+		logger.Error(" no user found", err.Error())
+		return c.Status(404).JSON(err.Error())
+	}
+
+	return c.JSON(people)
+}
+
+func init() { //creating routes
+	// grouping rotues
+
+	UserApi.Get(":id", getUser) // get single user
+
+	UserApi.Get("/", listUsers) //list all users
+
+	UserApi.Patch(":id", updateUser) //update user
+
+	UserApi.Delete(":id", deleteUser) //delete user
+
+	logger.Info("endpoint init api")
+}
---
+++ db/user.go
@@ -0,0 +1,103 @@
+package db
+
+import (
+	"emreddit/logger"
+	"errors"
+)
+
+type UserEntity struct {
+	ID       string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	Name     string `gorm:"column:name;not null;"`
+	Password string `gorm:"column:password;not null;"`
+	Email    string `gorm:"unique;not null;type:varchar(100);"`
+}
+
+// TableName overrides the table name used by User to `profiles`
+func (table *UserEntity) TableName() string {
+	return "users" //set TableName to operate
+}
+
+func CreateUser(user *UserEntity) error { //inserting user
+
+	if err := Db.Save(user).Error; err != nil { //checking for errors
+		return err
+	}
+
+	logger.Info(user)
+
+	return nil
+
+}
+
+func GetUserByEmailAndPassword(user *UserEntity) error { //checking login creds
+
+	if err := Db.Where(" email=? AND password=? ", user.Email, user.Password).First(&user).Error; err != nil {
+		logger.Info("Wrong Creds entry", err)
+		return err
+	}
+
+	return nil
+
+}
+
+func DeleteUser(id string) (string, error) { //delete User
+
+	var QueryResult = Db.Delete(&UserEntity{ID: id})
+
+	if err := QueryResult.Error; err != nil {
+
+		logger.Info("delete ", err)
+		return id, err
+
+	}
+	if QueryResult.RowsAffected == 0 { //checking affecting row to know if any operation took effect
+
+		logger.Info("USER IS NOT FOUND")
+
+		return id, errors.New("user NOT FOUND")
+	}
+
+	return id, nil
+}
+
+func ReadUser(id string, user *UserEntity) error {
+
+	user.ID = id //setting id
+
+	if err := Db.First(user).Error; err != nil { //checking for errors.
+		return err
+	}
+
+	return nil
+}
+
+func UpdateUser(user *UserEntity) error { //patchUpdating user
+
+	var result = Db.Updates(user)
+	if err := result.Error; err != nil { //checking for errors
+
+		logger.Error("err update", err)
+		return err
+
+	}
+
+	if result.RowsAffected == 0 { //check if any operation affects table
+		logger.Error("err user not found")
+
+		return errors.New("user NOT FOUND")
+	}
+
+	// Return the updated person
+	return nil
+
+}
+
+func GetUsers() ([]UserEntity, error) {
+	var users []UserEntity // creating person arr
+	if err := Db.Find(&users).Error; err != nil {
+		//check if err
+		return nil, err
+	}
+
+	return users, nil
+}
